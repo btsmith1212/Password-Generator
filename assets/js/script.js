a20d0b27641f83f96c57b41a40c7f6be3571ad5e
btsmith1212 <135077506+btsmith1212@users.noreply.github.com>
2023-07-06 23:43:21 -0500

    final draft pending revision, need more assistance

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,56 @@ var lowercaseArray = "abcdefghijklmnopqrstuvwxyz".split("");
 var numArray = "0123456789".split("");
 var specArray = "~`!@#$%^&*()_+=-?".split("");
 var masterArray = [...uppercaseArray, ...lowercaseArray, ...numArray, ...specArray];
+
 // added character arrays
+
+
+
 
 function generatePassword() {
   var password = "";
-  var wantsUppercase = "";
-
   var passwordLength = prompt("Password must be between 8 and 128 characters. Please select length.");
-
   var parsedLength = parseInt(passwordLength);
+  var uppercase = confirm("Do you want uppercase in your password.");
+  var lowercase = confirm("Do you want lowercase letters in your password?");
+  var numbers =  confirm("Do you want numbers in your password?");
+  var special = confirm("Do you want special characters in your password?");
 
+  function uppercase () {
+    var uppercase = confirm("Do you want uppercase in your password.");
+    if (uppercase) {
+      alert("Uppercase letters will be included in your password.");
+    } else {
+      alert("Uppercase letters won't be included in your password.");
+    }   
+  }
+  
+  function wantsLowercase () {
+    var lowercase = confirm("Do you want lowercase letters in your password?");
+    if (lowercase) {
+      alert("Lowercase letters will be included in your password.");
+    } else {
+      alert("Lowercase letters won't be included in your password.");
+    }
+  }
+  
+  function wantsNumbers () {
+    var numbers =  confirm("Do you want numbers in your password?");
+    if (numbers) {
+      alert("Numbers will be included in your password.");
+    } else {
+      alert("Numbers won't be included in your password.");
+  }
+  }
+  
+  function wantsSpecial () {
+  var special = confirm("Do you want special characters in your password?");
+  if (special) {
+      alert("Special characters will be included in your password.");
+    } else {
+      alert("Special characters won't be included in your password.");
+    }
+  }
 
   if (isNaN(parsedLength)) {
     alert("Please type in a number for password length.")
@@ -24,48 +64,34 @@ function generatePassword() {
   var max = 128;
 
   if (parsedLength < min || parsedLength > max) {
-  alert("Password must be between 8 and 128 characters.");
-  return null;
+      alert("Password must be between 8 and 128 characters.");
+      return null;
   }
 
-  var wantsUppercase = confirm("Do you want uppercase letters in your password?");
-  alert("Uppercase letters will be included in your password.");
-  var wantsLowercase = confirm("Do you want lowercase letters in your password?");
-  alert("Lowercase letters will be included in your password.");
-  var wantsNumbers = confirm("Do you want numbers in your password?");
-  alert("Numbers will be included in your password.");
-  var wantsSpecial = confirm("Do you want special characters in your password?");
-  alert("Special characters will be included in your password.");
-  alert("...generating password.");
-
-  wantsUppercase = parseInt(wantsUppercase);
-  if (wantsUppercase) {
+  if (uppercase) {
   masterArray = [...masterArray, ...uppercaseArray]; 
   }
 
-  wantsLowercase = parseInt(wantsLowercase)
-  if (wantsLowercase) {
+  if (lowercase) {
   masterArray = [...masterArray, ...lowercaseArray] 
   }
 
-  wantsNumbers = parseInt(wantsNumbers)
-  if (wantsNumbers) {
+  if (numbers) {
   masterArray = [...masterArray, ...numArray]
   }
 
-  wantsSpecial = parseInt(wantsSpecial)
-  if (wantsSpecial) {
+  if (special) {
   masterArray = [...masterArray, ...specArray]
   }
+  
   // merged arrays
   // added prompts for criteria
 
-  for (var i = 0; i < 1; i++) {
-    password = masterArray[Math.floor(Math.random() * masterArray.length)];
-    return ("")
+  for (var i = 0; i < parsedLength; i++) {
+    password = password + masterArray[Math.floor(Math.random() * masterArray.length)];
     // added function for password generation
   }
-
+  return password;
 }
 
 // Get references to the #generate element
@@ -74,7 +100,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword(password){
-  var password = generatePassword();
+  var password = generatePassword(); console.log(password);
   var pwTextArea = document.getElementById("password");
   pwTextArea.value = password;
   
